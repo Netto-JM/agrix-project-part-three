7b692abe77491e9222fb76f66f1324b118227d4f
NettoJM <nettojm86@gmail.com>
2023-10-03 00:32:25 -0300

    Implements interface "GrantedAuthority" on entity "Person"

--- src/main/java/com/betrybe/agrix/models/entities/Person.java
+++ src/main/java/com/betrybe/agrix/models/entities/Person.java
@@ -2,6 +2,7 @@ package com.betrybe.agrix.models.entities;
 
 
 import com.betrybe.agrix.security.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,7 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
  * Class representing a person.
  */
 @Entity
-public class Person implements UserDetails {
+public class Person implements UserDetails, GrantedAuthority {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -71,8 +72,9 @@ public class Person implements UserDetails {
   }
 
   @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
+  @JsonIgnore
+  public Collection<Person> getAuthorities() {
+    return List.of(this);
   }
 
   @Override
@@ -90,6 +92,12 @@ public class Person implements UserDetails {
 
   public void setRole(Role role) {
     this.role = role;
+  }
+
+  @JsonIgnore
+  @Override
+  public String getAuthority() {
+    return this.getRole().name();
   }
 
   @Override
